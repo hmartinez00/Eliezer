5f4e0d3a2662e5054e2280940b4077fc1dc7e942
hmartinez00 <hectoralonzomartinez00@gmail.com>
2023-04-18 13:02:57 -0400

    version estable 20230418

---
+++ whisper.py
@@ -0,0 +1,4 @@
+# !pip install git+https://github.com/openai/whisper.git
+# !pip install jiwer
+
+# !whisper "sonido.mp3" --task transcribe --model medium --verbose False --output_dir audio_transcription
